28272a67228bfbcfdb4ec58770be2643d464a691
PhiNSE <93396482+PhiNSE@users.noreply.github.com>
2024-08-09 21:46:46 +0700

    feat:deploy move port to env[PhiN]

--- src/main.ts
+++ src/main.ts
@@ -47,7 +47,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(8000);
+  await app.listen(process.env.PORT || 8000);
 
   logger.log(`Application is running on: ${await app.getUrl()}`);
 }
