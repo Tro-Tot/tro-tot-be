2b14dbdc6276176b22327deb6b5b75cfb319010a
Nguyen Huy Long <huylong2113@gmail.com>
2024-08-17 16:27:47 +0700

    feat: add find room with pagin [LongNH]

--- src/modules/room/room.controller.ts
+++ src/modules/room/room.controller.ts
@@ -9,6 +9,7 @@ import {
   UseGuards,
   UsePipes,
   ValidationPipe,
+  Query,
 } from '@nestjs/common';
 import { RoomService } from './room.service';
 import { CreateRoomDto } from './dto/create-room.dto';
@@ -37,9 +38,32 @@ export class RoomController {
     return this.roomService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.roomService.findOne(+id);
+  @Get('/house/:houseId')
+  findAllRoomByRoomId(
+    @Param('houseId') houseId: string,
+    @Query('page') page?: number,
+    @Query('page-size') pageSize?: number,
+    @Query('cursor') cursor?: string,
+    @Query('where') where?: string,
+    @Query('orderBy') orderBy?: string,
+  ) {
+    const params = {
+      page: page ? Number(page) : undefined,
+      pageSize: pageSize ? Number(pageSize) : undefined,
+      cursor: cursor ? JSON.parse(cursor) : undefined,
+      where: where ? JSON.parse(where) : undefined,
+      orderBy: orderBy ? JSON.parse(orderBy) : undefined,
+    };
+    return this.roomService.findAllRoomByRoomId(params, houseId);
+  }
+
+  //Test general where endpoint
+  @Get('/house/:houseId/Test')
+  findAllRoomByRoomIdTest(
+    @Param('houseId') houseId: string,
+    @Query() query?: Record<string, string>,
+  ) {
+    return this.roomService.findAllRoomByRoomIdGeneral(query, houseId);
   }
 
   @Patch(':id')
--- src/modules/room/room.service.ts
+++ src/modules/room/room.service.ts
@@ -1,8 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateRoomDto } from './dto/create-room.dto';
 import { UpdateRoomDto } from './dto/update-room.dto';
-import { Room } from '@prisma/client';
+import { Prisma, Room } from '@prisma/client';
 import { PrismaService } from 'prisma/prisma.service';
+import { apiSuccess } from 'src/common/dto/api-response';
+import { ApiResponse } from 'src/common/dto/response.dto';
 
 @Injectable()
 export class RoomService {
@@ -33,8 +35,107 @@ export class RoomService {
     }
   }
 
+  //TODO: Find all room by house id
   findAll() {
     return `This action returns all room`;
+  }
+
+  async findAllRoomByRoomId(
+    params: {
+      page?: number;
+      pageSize?: number;
+      cursor?: Prisma.RoomWhereUniqueInput;
+      where?: Prisma.RoomWhereInput;
+      orderBy?: Prisma.RoomOrderByWithRelationInput;
+    },
+    houseId: string,
+  ): Promise<ApiResponse> {
+    try {
+      let { page = 1, pageSize = 10, cursor, where, orderBy } = params;
+
+      // Ensure page and pageSize are valid
+      if (page < 1) page = 1;
+      if (pageSize < 1) pageSize = 10;
+
+      const skip = (page - 1) * pageSize;
+      const take = pageSize;
+
+      const [result, total] = await Promise.all([
+        this.prismaService.room.findMany({
+          skip,
+          take,
+          cursor,
+          where: {
+            ...where,
+            houseId: houseId,
+          },
+          orderBy,
+        }),
+        this.prismaService.room.count({
+          where: {
+            ...where,
+            houseId: houseId,
+          },
+        }),
+      ]);
+
+      return apiSuccess(200, { result, total }, 'Get rooms successfully');
+    } catch (error) {
+      console.error('Error fetching rooms:', error);
+      throw new Error('Failed to fetch rooms');
+    }
+  }
+
+  //Test general
+  async findAllRoomByRoomIdGeneral(
+    query: Record<string, string>,
+    houseId: string,
+  ): Promise<ApiResponse> {
+    try {
+      const { page, pageSize, cursor, orderBy, ...filters } = query;
+
+      // Ensure page and pageSize are valid
+      const parsedPage = page ? parseInt(page, 10) : 1;
+      const parsedPageSize = pageSize ? parseInt(pageSize, 10) : 10;
+      if (
+        isNaN(parsedPage) ||
+        isNaN(parsedPageSize) ||
+        parsedPage < 1 ||
+        parsedPageSize < 1
+      ) {
+        throw new HttpException(
+          'Invalid page or pageSize',
+          HttpStatus.BAD_REQUEST,
+        );
+      }
+
+      const skip = (parsedPage - 1) * parsedPageSize;
+      const take = parsedPageSize;
+
+      const whereCondition: Prisma.RoomWhereInput = { houseId };
+
+      Object.entries(filters).forEach(([key, value]) => {
+        if (key in Prisma.RoomScalarFieldEnum) {
+          whereCondition[key] = value;
+        }
+      });
+
+      const [result, total] = await Promise.all([
+        this.prismaService.room.findMany({
+          skip,
+          take,
+          where: whereCondition,
+          orderBy: orderBy ? JSON.parse(orderBy) : undefined,
+          include: { house: true },
+        }),
+        this.prismaService.room.count({ where: whereCondition }),
+      ]);
+
+      return apiSuccess(200, { result, total }, 'Get rooms successfully');
+    } catch (error) {
+      console.error('Error fetching rooms:', error);
+      throw new Error('Failed to fetch rooms');
+    }
   }
 
   findOne(id: number) {
