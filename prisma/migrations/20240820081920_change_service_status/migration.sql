e1765f1afd1b11128209d4e310a705a72ffe68cc
Nguyen Huy Long <huylong2113@gmail.com>
2024-08-20 15:19:51 +0700

    fix: fix schema conflict [LongNH]

---
+++ prisma/migrations/20240820081920_change_service_status/migration.sql
@@ -0,0 +1,19 @@
+/*
+  Warnings:
+
+  - The values [ACTIVE] on the enum `ServiceStatus` will be removed. If these variants are still used in the database, this will fail.
+
+*/
+-- AlterEnum
+BEGIN;
+CREATE TYPE "ServiceStatus_new" AS ENUM ('AVAILABLE', 'UNAVAILABLE', 'UNREGISTRABLE', 'TERMINATED');
+ALTER TABLE "services" ALTER COLUMN "status" DROP DEFAULT;
+ALTER TABLE "services" ALTER COLUMN "status" TYPE "ServiceStatus_new" USING ("status"::text::"ServiceStatus_new");
+ALTER TYPE "ServiceStatus" RENAME TO "ServiceStatus_old";
+ALTER TYPE "ServiceStatus_new" RENAME TO "ServiceStatus";
+DROP TYPE "ServiceStatus_old";
+ALTER TABLE "services" ALTER COLUMN "status" SET DEFAULT 'AVAILABLE';
+COMMIT;
+
+-- AlterTable
+ALTER TABLE "services" ALTER COLUMN "status" SET DEFAULT 'AVAILABLE';
