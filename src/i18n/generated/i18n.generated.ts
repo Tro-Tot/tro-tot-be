fbc1963e83961825149a3d93b84b79dcd9ff89af
PhiNSE <93396482+PhiNSE@users.noreply.github.com>
2024-08-22 21:58:53 +0700

    fix: this. error, generate [PhiN]

--- src/i18n/generated/i18n.generated.ts
+++ src/i18n/generated/i18n.generated.ts
@@ -5,6 +5,11 @@
 import { Path } from "nestjs-i18n";
 /* prettier-ignore */
 export type I18nTranslations = {
+    "house": {
+        "house_not_exist": string;
+        "house_exist": string;
+        "house_not_belong_to_staff": string;
+    };
     "room": {
         "room_not_exist": string;
         "room_exist": string;
--- src/modules/house/pipe/is-house-of-staff.ts
+++ src/modules/house/pipe/is-house-of-staff.ts
@@ -1,17 +1,23 @@
 import {
-  PipeTransform,
-  Injectable,
   ArgumentMetadata,
   BadRequestException,
+  Injectable,
+  PipeTransform,
 } from '@nestjs/common';
+import { I18nContext } from 'nestjs-i18n';
+import { I18nTranslations } from 'src/i18n/generated/i18n.generated';
 import { HouseService } from '../house.service';
 
 @Injectable()
 export class IsHouseOfStaff implements PipeTransform {
   constructor(private readonly houseService: HouseService) {}
   transform({ houseId, staffId }: any, metadata: ArgumentMetadata) {
-    if (this.houseService.isHouseOfStaff(houseId, staffId)) {
-      throw new BadRequestException('Nhà không thuộc quyền quản lý của bạn');
+    if (!this.houseService.isHouseOfStaff(houseId, staffId)) {
+      throw new BadRequestException(
+        I18nContext.current<I18nTranslations>().t(
+          'house.house_not_belong_to_staff',
+        ),
+      );
     }
     return houseId;
   }
